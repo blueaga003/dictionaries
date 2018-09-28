6a6848e2f4bd5fe2f67de7991dc7494cf814467f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-27 19:53:20 -0700

    Create word_counts function

---
+++ wordcount.py
@@ -0,0 +1,20 @@
+# put your code here.
+import sys 
+
+import_file = open(sys.argv[1], "r")
+
+def word_counts(file_1):
+	words = {}
+
+	for line in file_1:
+		line = line.strip().split()
+		for word in line:
+			if word in words:
+				words[word] += 1
+			else:
+				words[word] = 1
+
+	return words
+
+dictionary_of_word_counts = word_counts(import_file)
+print(dictionary_of_word_counts)
