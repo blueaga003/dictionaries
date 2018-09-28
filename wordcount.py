11bf577dc683fccb62b8e434bbe8c1e9b7081b83
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-27 20:02:32 -0700

    Optimizing word_counts function

--- wordcount.py
+++ wordcount.py
@@ -5,15 +5,11 @@ import_file = open(sys.argv[1], "r")
 
 def word_counts(file_1):
 	words = {}
-
 	for line in file_1:
 		line = line.strip().split()
 		for word in line:
 			if word in words:
-				words[word] += 1
-			else:
-				words[word] = 1
-
+				words[word] = word.get(words, 0) + 1  
 	return words
 
 dictionary_of_word_counts = word_counts(import_file)
